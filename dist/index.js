09852780e2d610b25b3ad267da80bbf020b02c68
Diana Vaher <146828798+vaherdiana@users.noreply.github.com>
2024-05-02 09:48:28 +0300

    add regex correct grouping

--- dist/index.js
+++ dist/index.js
@@ -390,7 +390,7 @@ try {
     console.log('Configuring deployment key(s)');
 
     child_process.execFileSync(sshAddCmd, ['-L']).toString().trim().split(/\r?\n/).forEach(function(key) {
-        const parts = key.match(\b(github\.com[:])([_.a-z0-9-]+\/[_.a-z0-9-]+);
+        const parts = key.match(/\b(github\.com[:])([_.a-z0-9-]+\/[_.a-z0-9-]+)/i);
 
         if (!parts) {
             if (logPublicKey) {
